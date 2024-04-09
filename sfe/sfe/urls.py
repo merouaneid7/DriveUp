28945a091d565e0d63ec24f2325199c8e06060e4
merouane <ikkisauto@gmail.com>
2024-04-09 16:29:42 +0000

    user validation by admin

--- sfe/sfe/urls.py
+++ sfe/sfe/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('authentication/signup',views.signup,name="signup"),
     path('authentication/login',views.user_login,name="login"),
+    path('userlist',views.uerslist,name="uerslist"),
+    path('approve_user/<int:user_id>',views.approve_user,name="approve_user")
 ]
--- sfe/sfe/views.py
+++ sfe/sfe/views.py
@@ -2,11 +2,27 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import login
 from . EmailBackEnd import EmailBackEnd
+from django.http import HttpResponse
 
 def home(request):
     return render(request,"main/home.html")
+
+def uerslist(request):
+    users = User.objects.all()
+    context = {
+        'users' : users,
+    }
+    return render(request,"userslist.html",context)
+
+
+def approve_user(request,user_id):
+    user = User.objects.get(pk=user_id)
+    user.is_active=True
+    user.save()
+    return HttpResponse("DONE")
+    
 
 
 def user_login(request):
@@ -17,7 +33,7 @@ def user_login(request):
         print(password)
         user = EmailBackEnd.authenticate(request, username=email, password=password)
         print(user)
-        if user is not None:
+        if user is not None and user.is_active is True:
             login(request, user)
             return redirect('home')  
         else:
@@ -28,7 +44,6 @@ def user_login(request):
 
 
 def signup(request):
-
     if request.method == "POST":
         username = request.POST.get('username')
         first_name=request.POST.get('first_name')
@@ -36,20 +51,17 @@ def signup(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        
-        # check email
         if User.objects.filter(email=email).exists():
            
            messages.warning(request,'Email are Already Exists !')
            return redirect('signup')
-        
 
-        # check username
         if User.objects.filter(username=username).exists():
            messages.warning(request,'Username are Already exists !')
            return redirect('signup')
         
         user = User.objects.create_user(username=username,email=email,password=password)
+        user.is_active=False
         user.save()
         return redirect('login')
     return render(request,'auth/signup.html')
